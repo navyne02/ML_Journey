2d376a8389b5ff294b56a15b7a13b1114a1bda7b
navyne02 <naveenks1003@gmail.com>
2026-04-02 23:01:23 +0530

    commit

---
+++ day3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Creating a NumPy Array
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Normal Array:", arr)
+
+# Fast Math: Square all numbers in one go!
+print("Squared Numbers:", arr ** 2)
