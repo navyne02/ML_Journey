0064cb58f1cfc8a6159bb65b5bdd9d83cd1ac12d
navyne02 <naveenks1003@gmail.com>
2026-05-01 16:19:42 +0530

    commit

--- day2.py
+++ day2.py
@@ -1,4 +1,9 @@
-import pandas as pd
+import pandas as pd 
 data = {'Product': ['Laptop', 'Mouse'], 'Price': [50000, 1000]}
 df = pd.DataFrame(data)
 print(df)
+
+# pandas can be easy to handle the data
+# Creating the dictionary Product and Price
+# 2D table it can be convert this is called df
+# It can be print(df)
