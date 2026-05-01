db661191ab0221286ba7824828dd0afc31eb0e84
navyne02 <naveenks1003@gmail.com>
2026-05-01 16:29:07 +0530

    commit

--- day3.py
+++ day3.py
@@ -6,4 +6,8 @@ arr = np.array([1, 2, 3, 4, 5])
 print("Normal Array:", arr)
 
 # Fast Math: Square all numbers in one go!
-print("Squared Numbers:", arr ** 2)
+print("Squared Numbers:", arr ** 2) 
+
+# importing numpy as np 
+# storing values into arr = np.array
+# Printing normal array and Squared arrays
